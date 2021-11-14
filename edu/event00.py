2ecae9b16809520107c289be2007c5b6e6db2ce3
dan-hi <63167529+dan-hi@users.noreply.github.com>
2021-11-13 19:56:59 -1000

    Create event00.py

---
+++ edu/event00.py
@@ -0,0 +1,23 @@
+def on chat):
+  agent. set_item (CARROTS, 64, 1)
+  for index in range(3):
+    for index2 in range (5):
+      agent. till (FORWARD)
+      agent. move (FORWARD, 1)
+      agent.place (DOWN)
+     agent. move(BACK, 5)
+     agent.move(RIGHT, 2)
+      
+player.on_chat("carrot", on_chat)
+
+def on chat2() :
+  for index3
+  in range (15):
+  mobs. spawn (CHICKEN, pos(0, 0, 0))
+  0.
+player. on chat ("chicken"
+on chat2)
+def
+on_chat3 () :
+for index4 in range (15):
+mobs. spawn (SNOWBALL_ PROJECTILE _MOB,
