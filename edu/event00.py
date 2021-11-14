48561add63e958583cf0d215e40a5f6baff2875b
dan-hi <63167529+dan-hi@users.noreply.github.com>
2021-11-13 20:16:52 -1000

    Update event00.py

--- edu/event00.py
+++ edu/event00.py
@@ -1,11 +1,11 @@
-def on chat):
-  agent. set_item (CARROTS, 64, 1)
+def on chat():
+  agent.set_item (CARROTS, 64, 1)
   for index in range(3):
     for index2 in range (5):
-      agent. till (FORWARD)
-      agent. move (FORWARD, 1)
+      agent.till (FORWARD)
+      agent.move (FORWARD, 1)
       agent.place (DOWN)
-    agent. move(BACK, 5)
+    agent.move(BACK, 5)
     agent.move(RIGHT, 2)
       
 player.on_chat("carrot", on_chat)
@@ -13,7 +13,6 @@ player.on_chat("carrot", on_chat)
 def on chat2() :
   for index3 in range (15):
   mobs.spawn (CHICKEN, pos(0, 0, 0))
-  0.
 
 player.on_chat ("chicken", on_chat2)
 
@@ -23,8 +22,8 @@ def on_chat3 () :
 
 player.on_chat ("snowball", on_chat3)
 def item_interacted_blaze_rod() :
-  player.run_chat_command( "carrot")
-  player.run_chat_command ( "chicken"')
+  player.run_chat_command("carrot")
+  player.run_chat_command("chicken")
   player.run_chat_command("snowball")
 
 player.on_item_interacted (BLAZE_ROD, item_interacted_blaze_rod)
